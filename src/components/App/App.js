de278c0e60699c27467f6c9d93e7676937de9223
Nicole Forseth <forseth.nico@gmail.com>
2022-10-27 21:53:23 -0600

    Update styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { fetchJoke } from '../../apiCalls';
 import './App.css'
 import Joke from '../Joke/Joke';
+import dad from '../../assets/dad.png'
 
 class App extends Component {
     constructor() {
@@ -31,6 +32,11 @@ class App extends Component {
         return (
           <main className = 'App'>
             <h1>DAD·A·BASE</h1>
+            <img
+              className="dad-backdrop"
+              src={dad}
+              alt="retro dad"
+            />
             {!this.state.error && <Joke joke={this.state.joke} handleClick={this.handleClick}/>}
             {this.state.error && <h4 className="error-message">{this.state.error}</h4>}         
           </main>
